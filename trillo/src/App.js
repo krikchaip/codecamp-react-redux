632f443c22e1f0f62350c794c1bce3907860f28e
krikchaip <krikchai.p@gmail.com>
2019-11-15 12:04:09 +0700

    finished trillo

--- trillo/src/App.js
+++ trillo/src/App.js
@@ -1,7 +1,32 @@
 import React from 'react'
 
+import Item from './components/Item'
+
+const data = [
+  'Subwoofer',
+  'Non-porous, washable',
+  'Wings',
+  'Beveled Bezel',
+  'Beveled Bezel',
+  'Seedless'
+]
+
 function App() {
-  return <div>Hello World!</div>
+  return (
+    <div className="App">
+      <div className="Trillo">
+        <div style={{ fontWeight: 'bold' }}>Phone Features</div>
+        <div className="Container">
+          {data.map((str, idx) => (
+            <Item key={idx}>{str}</Item>
+          ))}
+        </div>
+        <div style={{ color: '#7f807f', fontWeight: 'bold' }}>
+          Add a card...
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ trillo/src/components/Item.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Item(props) {
+  return <div className="Item">{props.children}</div>
+}
+
+export default Item
