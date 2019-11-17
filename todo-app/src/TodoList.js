048632c44b6c435260b6172cea3e8cd5e21679ed
krikchaip <krikchai.p@gmail.com>
2019-11-17 23:27:03 +0700

    todo app - init

---
+++ todo-app/src/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './TodoList.module.css'
+
+class TodoList extends React.Component {
+  render = () => {
+    return <div>Todo</div>
+  }
+}
+
+export default TodoList
