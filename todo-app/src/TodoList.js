b7f0bee2f2ca8cf0995b6628c670c8912eedcfd9
krikchaip <krikchai.p@gmail.com>
2019-11-20 03:44:04 +0700

    finished api

--- todo-app/src/TodoList.js
+++ todo-app/src/TodoList.js
@@ -5,10 +5,19 @@ import styles from './TodoList.module.css'
 import NewTodo from './components/NewTodo'
 import Todo from './components/Todo'
 
+const API = 'http://localhost:3001'
+
 class TodoList extends React.Component {
   state = {
     textValue: '',
-    todos: [{ ticked: false, name: 'KILL DA BEECH' }]
+    todos: [] // { id: string, ticked: Boolean, name: String }
+  }
+
+  componentDidMount = async () => {
+    const response = await fetch(`${API}/todos`)
+    const data = await response.json()
+
+    this.setState({ todos: data })
   }
 
   handleChange = e => {
@@ -17,29 +26,45 @@ class TodoList extends React.Component {
     })
   }
 
-  handleAdd = () => {
+  handleAdd = async () => {
     if (!this.state.textValue) return
-    this.setState(
-      state => ({
-        todos: state.todos.concat({ ticked: false, name: state.textValue })
-      }),
-      () => {
-        this.setState({ textValue: '' })
-      }
-    )
-  }
 
-  handleTick = idx => () => {
+    const todo = { ticked: false, name: this.state.textValue }
+    await fetch(`${API}/todos`, {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify(todo)
+    })
+
     this.setState(state => ({
-      todos: state.todos.map((todo, todoIdx) =>
-        todoIdx === idx ? { ...todo, ticked: !todo.ticked } : todo
-      )
+      todos: state.todos.concat(todo),
+      textValue: ''
     }))
   }
 
-  handleDelete = idx => () => {
+  handleTick = id => async () => {
+    const tickedTodo = this.state.todos.find(x => x.id === id)
+    tickedTodo.ticked = !tickedTodo.ticked
+
+    await fetch(`${API}/todos/${id}`, {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'PUT',
+      body: JSON.stringify(tickedTodo)
+    })
+
     this.setState(state => ({
-      todos: state.todos.filter((todo, todoIdx) => todoIdx !== idx)
+      todos: state.todos.map(todo => (todo.id === id ? tickedTodo : todo))
+    }))
+  }
+
+  handleDelete = id => async () => {
+    await fetch(`${API}/todos/${id}`, {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'DELETE'
+    })
+
+    this.setState(state => ({
+      todos: state.todos.filter(todo => todo.id !== id)
     }))
   }
 
@@ -51,13 +76,13 @@ class TodoList extends React.Component {
           onChange={this.handleChange}
           onAdd={this.handleAdd}
         />
-        {this.state.todos.map(({ ticked, name }, idx) => (
+        {this.state.todos.map(({ ticked, name, id }) => (
           <Todo
-            key={idx}
+            key={id}
             ticked={ticked}
             name={name}
-            onTick={this.handleTick(idx)}
-            onDelete={this.handleDelete(idx)}
+            onTick={this.handleTick(id)}
+            onDelete={this.handleDelete(id)}
           />
         ))}
       </div>
