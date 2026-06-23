71106fffeb663dd7408364ea843f5b98b75fd06f
Krikchai Pongtaveewould <krikchai.p@gmail.com>
2026-06-24 03:38:08 +0700

    Replace CRA with Vite

--- todo-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import './index.css'
-
-import TodoList from './TodoList'
-
-ReactDOM.render(<TodoList />, document.getElementById('root'))
